93e9f09e2dbdadf4ec4d4a0bdd1da61b1dcda213
Abubakr <bkarali345@gmail.com>
2026-02-19 16:32:06 +0200

    Make mainStore the main page and restructure project

---
+++ src/app/pageeee.tsx
@@ -0,0 +1,13 @@
+// "use client";
+// import React from "react";
+// import PageMain from "./mainStore/page";
+
+// const Page = () => {
+//   return (
+//     <div className="w-full">
+//       <PageMain />
+//     </div>
+//   );
+// };
+
+// export default Page;
